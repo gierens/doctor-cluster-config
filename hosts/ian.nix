644a75ea9a2836fb6aedb26463bb912f2bdb8f1c
Sandro-Alessio Gierens <sandro@gierens.de>
2025-02-09 14:14:19 +0100

    feat: install pkvm kernel on ian
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/ian.nix
+++ hosts/ian.nix
@@ -2,7 +2,7 @@
   imports = [
     ../modules/hardware/poweredge-r760.nix
     ../modules/nfs/client.nix
-    ../modules/intel_tdx.nix
+    ../modules/intel_pkvm.nix
   ];
 
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNS0W800690";
