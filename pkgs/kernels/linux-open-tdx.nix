3fd3469e8d40c8ae9ea633ed04d340ca02b88c8c
Sandro-Alessio Gierens <sandro@gierens.de>
2025-08-23 14:57:15 +0200

    tdx: add open-tdx kernel
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ pkgs/kernels/linux-open-tdx.nix
@@ -0,0 +1,45 @@
+{ buildLinux, fetchFromGitHub, ... }@args:
+let
+  buildKernel = { owner, repo, rev, sha256, version, modDirVersion, extraPatches ? [ ] }:
+    buildLinux
+      (args // rec {
+        inherit version modDirVersion;
+
+        src = fetchFromGitHub {
+          inherit owner repo rev sha256;
+        };
+
+        kernelPatches = [
+          {
+            name = "open-tdx-config";
+            patch = null;
+            extraConfig = ''
+              KVM_SW_PROTECTED_VM y
+              KVM_GENERIC_PRIVATE_MEM y
+              SYSTEM_TRUSTED_KEYS y
+              SYSTEM_REVOCATION_KEYS y
+            '';
+          }
+          # NOTE: only needed prior to 6.9
+          # {
+          #   name = "bug_func export";
+          #   patch = ./zfs-tdx.patch;
+          #   extraConfig = '''';
+          # }
+        ] ++ extraPatches;
+        extraMeta.branch = version;
+        ignoreConfigErrors = true;
+      } // (args.argsOverride or { }));
+
+  open_tdx_6_8 = {
+    owner = "sslab-gatech";
+    repo = "tdx-linux";
+    # branch: opentdx-host
+    rev = "fe61e02ba4eed7b193d9f4578614732cc6f34055";
+    sha256 = "";
+    version = "6.8";
+    modDirVersion = "6.8.0";
+  };
+in
+# change here to change kernel
+buildKernel open_tdx_6_8
