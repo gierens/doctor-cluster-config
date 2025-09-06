ff588a1a7bdf48414fa998cad4aab535e084c140
Sandro-Alessio Gierens <sandro@gierens.de>
2025-09-06 19:01:04 +0200

    tdx: use zfs unstable for linux v6.16.2 with tdx support
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- modules/linux-tdx-6-16.nix
+++ modules/linux-tdx-6-16.nix
@@ -4,6 +4,7 @@ let
   linuxPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux.linux);
 in
 {
+  boot.zfs.package = lib.mkForce pkgs.zfsUnstable;
   boot.kernelPackages = lib.mkForce linuxPackages;
   boot.kernelParams = [
     "kvm_intel.tdx=1"
